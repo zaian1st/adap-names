edb2238937382e242747aa287a45411e7862632b
zaian1st <59979965+zaian1st@users.noreply.github.com>
2025-10-27 01:46:50 +0300

    Aufgabe B01 erledigt

--- src/adap-b01/coordinates/Coordinate.ts
+++ src/adap-b01/coordinates/Coordinate.ts
@@ -3,14 +3,17 @@ export class Coordinate {
     private x: number = 0;
     private y: number = 0;
 
+    /** @methodtype initialization-method */
     constructor(x?: number, y?: number) {
         this.initialize(x, y);
     }
 
+    /** @methodtype factory-method */
     public static createOrigin(): Coordinate {
         return new Coordinate(0, 0);
     }
 
+    /** @methodtype initialization-method */
     public initialize(x?: number, y?: number): void {
         if (x != undefined) {
             this.setX(x);
@@ -21,18 +24,22 @@ export class Coordinate {
         }
     }
 
+    /** @methodtype conversion-method */
     public toString(): string {
         return this.asDataString();
     }
 
+    /** @methodtype conversion-method */
     public asDataString(): string {
         return this.getX() + "#" + this.getY();
     }
 
+    /** @methodtype boolean-query-method */
     public isEqual(other: Coordinate): boolean {
         return (this.getX() == other.getX()) && (this.getY() == other.getY());
     }
 
+    /** @methodtype get-method */
     public getHashCode(): number {
         let hashCode: number = 0;
         const s: string = this.asDataString();
@@ -44,9 +51,10 @@ export class Coordinate {
         return hashCode;
     }
 
+    /** @methodtype comparison-method */
     public compareDistance(other: Coordinate): number {
         let thisR = Math.hypot(this.getX(), this.getY());
-        let otherR = Math.hypot(this.getX(), this.getY());
+        let otherR = Math.hypot(other.getX(), other.getY());
         if (thisR == otherR) {
             return 0;
         } else if (thisR < otherR) {
@@ -56,6 +64,7 @@ export class Coordinate {
         }
     }
 
+    /** @methodtype convenience-method */
     public reset(): void {
         this.initialize(0, 0);
     }
@@ -65,22 +74,26 @@ export class Coordinate {
         return this.x;
     }
     
+    /** @methodtype set-method */
     public setX(x: number) {
         this.x = x;
     }
     
+    /** @methodtype get-method */
     public getY(): number {
         return this.y;
     }
 
+    /** @methodtype set-method */
     public setY(y: number) {
         this.y = y;
     }
 
+    /** @methodtype query-method */
     public calcStraightLineDistance(other: Coordinate): number {
         let deltaX: number = Math.abs(other.getX() - this.getX());
         let deltaY: number = Math.abs(other.getY() - this.getY());
         return Math.hypot(deltaX, deltaY);
     }
 
- }
+}
--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -18,56 +18,82 @@ export class Name {
     private delimiter: string = DEFAULT_DELIMITER;
     private components: string[] = [];
 
-    /** Expects that all Name components are properly masked */
+    /** 
+     * @methodtype initialization-method 
+     * Expects that all Name components are properly masked 
+     */
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        this.components = other;
+        if (delimiter !== undefined) {
+            this.delimiter = delimiter;
+        }
     }
 
     /**
+     * @methodtype conversion-method
      * Returns a human-readable representation of the Name instance using user-set special characters
      * Special characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(delimiter);
     }
 
     /** 
+     * @methodtype conversion-method
      * Returns a machine-readable representation of Name instance using default special characters
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The special characters in the data string are the default characters
      */
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(DEFAULT_DELIMITER);
     }
 
-    /** Returns properly masked component string */
+    /** 
+     * @methodtype get-method
+     * Returns properly masked component string 
+     */
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        return this.components[i];
     }
 
-    /** Expects that new Name component c is properly masked */
+    /** 
+     * @methodtype set-method
+     * Expects that new Name component c is properly masked 
+     */
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components[i] = c;
     }
 
-     /** Returns number of components in Name instance */
-     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+    /** 
+     * @methodtype get-method
+     * Returns number of components in Name instance 
+     */
+    public getNoComponents(): number {
+        return this.components.length;
     }
 
-    /** Expects that new Name component c is properly masked */
+    /** 
+     * @methodtype command-method
+     * Expects that new Name component c is properly masked 
+     */
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(i, 0, c);
     }
 
-    /** Expects that new Name component c is properly masked */
+    /** 
+     * @methodtype command-method
+     * Expects that new Name component c is properly masked 
+     */
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c);
     }
 
+    /** 
+     * @methodtype command-method
+     */
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(i, 1);
     }
 
 }
